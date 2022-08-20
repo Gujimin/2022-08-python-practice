54af01ea57cd14003b58a6b1016b1d1bf22ab76e
Gujimin <111211617+Gujimin@users.noreply.github.com>
2022-08-20 14:03:20 +0900

    Add files via upload

---
+++ 정수 입력.py
@@ -0,0 +1,14 @@
+a = int(input("정수1 입력>>>"))
+b = int(input("정수2 입력>>>"))
+c = int(input("정수3 입력>>>"))
+
+if a>b and a>c:
+    print("가장 큰수는",a,"입니다.")
+elif b>a and b>c:
+    print("가장 큰수는", b, "입니다.")
+else:
+    print("가장 큰수는", c, "입니다.")
+
+
+
+
---
+++ 학점.py
@@ -0,0 +1,11 @@
+score = int(input("점수를 입력하세요:"))
+if score >=90:
+     print("점수는",score,"점이고 학점은 A학점입니다.")
+elif score >=80:
+     print("점수는",score,"점이고 학점은 B학점입니다.")
+elif score >=70:
+     print("점수는", score,"점이고 학점은 C학점입니다.")
+elif score >=60:
+     print("점수는",score,"점이고 학점은 D학점입니다.")
+else:
+     print("점수는",score,"점이고 학점은 F학점입니다.")
