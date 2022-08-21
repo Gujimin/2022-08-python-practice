e7b68f0f49dcba92caf66e53f21b7ce94e942df8
Gujimin <111211617+Gujimin@users.noreply.github.com>
2022-08-21 14:35:13 +0900

    Add files via upload

---
+++ 구구단 가로.py
@@ -0,0 +1,8 @@
+
+
+for a in range(1,10):
+    for x in range(2, 10):
+        print("{}*{}={}".format(a, x, a * x), end='\t')
+    print()
+
+
---
+++ 임의의 양수.py
@@ -0,0 +1,7 @@
+a = int(input("임의의 정수를 입력하세요>>>"))
+sum = 0
+i = 1
+while i <= a:
+    sum += i
+    i += 1
+print(f'1부터 {a}까지의 정수의 합은 {sum} 입니다.')
